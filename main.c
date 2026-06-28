63083a709f508bcaf113b82459afbd964a92012c
Shane <shaneshaji10@gmail.com>
2026-06-28 13:10:59 +0530

    handle duplicate keys & binary insertion

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ typedef struct {
 } InputBuffer;
 
 
-typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
+typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL, EXECUTE_DUPLICATE_KEY} ExecuteResult;
 
 typedef enum{ META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND } MetaCommandResult;
 
@@ -105,7 +105,13 @@ uint32_t* leaf_node_value(void* node, uint32_t cell_num){
     return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
 }
 
+void set_node_type(void* node, NodeType type){
+    uint8_t value = type;
+    *((uint8_t*)(node+NODE_TYPE_OFFSET)) = value;
+}
+
 void initialize_leaf_node(void* node){
+    set_node_type(node, NODE_LEAF);
     *leaf_node_num_cells(node) = 0;
 }
 
@@ -142,20 +148,54 @@ Cursor* table_start(Table* table){
     return cursor;
 }
 
-Cursor* table_end(Table* table){
+
+
+Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key){
+    void* node = get_page(table->pager, page_num);
+    uint32_t num_cells = *leaf_node_num_cells(node);
+
     Cursor* cursor = malloc(sizeof(Cursor));
     cursor->table = table;
-    cursor->page_num = table->root_page_num;
+    cursor->page_num = page_num;
 
-    void* root_node = get_page(table->pager, table->root_page_num);
-    uint32_t num_cells = *leaf_node_num_cells(root_node);
-    cursor->cell_num = num_cells;
-    cursor->end_of_table = true;
+    // Binary search
+    uint32_t min_index = 0;
+    uint32_t one_past_max_index = num_cells;
+    while (one_past_max_index != min_index) {
+        uint32_t index = (min_index + one_past_max_index) / 2;
+        uint32_t key_at_index = *leaf_node_key(node, index);
+        if (key == key_at_index) {
+            cursor->cell_num = index;
+            return cursor;
+        }
+        if (key < key_at_index) {
+            one_past_max_index = index;
+        } else {
+            min_index = index + 1;
+        }
+    }
 
+    cursor->cell_num = min_index;
     return cursor;
 }
 
+NodeType get_node_type(void* node){
+    uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+    return (NodeType) value;
+}
 
+Cursor* table_find(Table* table, uint32_t key){
+    uint32_t root_page_num = table->root_page_num;
+
+    void* root_node = get_page(table->pager, root_page_num);
+
+    if (get_node_type(root_node) == NODE_LEAF){
+        return leaf_node_find(table, root_page_num, key);
+    } else{
+        printf("No need implement searching on an internal node\n");
+        exit(EXIT_FAILURE);
+    }
+}
 void cursor_advance(Cursor* cursor){
     uint32_t page_num = cursor->page_num;
     void* node = get_page(cursor->table->pager, page_num);
@@ -416,11 +456,20 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 
 ExecuteResult execute_insert(Statement* statement, Table* table){
     void* node = get_page(table->pager, table->root_page_num);
-    if ((*leaf_node_num_cells(node)) >= LEAF_NODE_MAX_CELLS){
+    uint32_t num_cells = (*leaf_node_num_cells(node));
+    if (num_cells>=LEAF_NODE_MAX_CELLS){
         return EXECUTE_TABLE_FULL;
     }
     Row* row_to_insert = &(statement->row_to_insert);
-    Cursor* cursor = table_end(table);
+    uint32_t key_to_insert = row_to_insert->id;
+    Cursor* cursor = table_find(table, key_to_insert);
+
+    if (cursor->cell_num < num_cells){
+        uint32_t key_at_index = *leaf_node_key(node, cursor->cell_num);
+        if (key_at_index == key_to_insert){
+            return EXECUTE_DUPLICATE_KEY;
+        }
+    }
     leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
     free(cursor);
     return EXECUTE_SUCCESS;
@@ -462,55 +511,58 @@ void read_input(InputBuffer* input_buffer){
 
 
 int main(int argc, char* argv[]){
-    if (argc < 2){
-        printf("Must supply a database filename.\n");
-        exit(EXIT_FAILURE);
+if (argc < 2){
+    printf("Must supply a database filename.\n");
+    exit(EXIT_FAILURE);
+}
+
+char* filename = argv[1];
+Table* table = db_open(filename);
+InputBuffer* input_buffer = new_input_buffer();
+while (true){
+
+print_prompt();
+read_input(input_buffer);
+
+if (input_buffer->buffer[0] == '.'){
+    switch (do_meta_command(input_buffer, table)) {
+        case META_COMMAND_SUCCESS:
+        continue;
+        case META_COMMAND_UNRECOGNIZED_COMMAND:
+        printf("Unrecognized meta command '%s'\n", input_buffer->buffer);
+        continue;
     }
+}
 
-    char* filename = argv[1];
-    Table* table = db_open(filename);
-    InputBuffer* input_buffer = new_input_buffer();
-    while (true){
+Statement statement;
+switch (prepare_statement(input_buffer, &statement)) {
+    case PREPARE_SUCCESS:
+    break;
+    case PREPARE_NEGATIVE_ID:
+    printf("ID must be a positive number");
+    continue;
+    case PREPARE_STRING_TOO_LONG:
+    printf("String is too long");
+    continue;
+    case PREPARE_SYNTAX_ERROR:
+    printf("Unexpected Syntax error. Could not parse statement.\n");
+    continue;
+    case PREPARE_UNRECOGNIZED_STATEMENT:
+    printf("Unrecognized Keyword at start of '%s'\n", input_buffer->buffer);
+    continue;
+}
 
-        print_prompt();
-        read_input(input_buffer);
+switch (execute_statement(&statement, table)) {
+case (EXECUTE_SUCCESS):
+printf("Executed.\n");
+break;
+case (EXECUTE_TABLE_FULL):
+printf("Error: Table full.\n");
+break;
+case(EXECUTE_DUPLICATE_KEY):
+printf("Error: Duplicate Key\n");
+break;
+}
 
-        if (input_buffer->buffer[0] == '.'){
-            switch (do_meta_command(input_buffer, table)) {
-                case META_COMMAND_SUCCESS:
-                continue;
-                case META_COMMAND_UNRECOGNIZED_COMMAND:
-                printf("Unrecognized meta command '%s'\n", input_buffer->buffer);
-                continue;
-            }
-        }
-
-        Statement statement;
-        switch (prepare_statement(input_buffer, &statement)) {
-            case PREPARE_SUCCESS:
-            break;
-            case PREPARE_NEGATIVE_ID:
-            printf("ID must be a positive number");
-            continue;
-            case PREPARE_STRING_TOO_LONG:
-            printf("String is too long");
-            continue;
-            case PREPARE_SYNTAX_ERROR:
-            printf("Unexpected Syntax error. Could not parse statement.\n");
-            continue;
-            case PREPARE_UNRECOGNIZED_STATEMENT:
-            printf("Unrecognized Keyword at start of '%s'\n", input_buffer->buffer);
-            continue;
-        }
-
-        switch (execute_statement(&statement, table)) {
-            case (EXECUTE_SUCCESS):
-            printf("Executed.\n");
-            break;
-            case (EXECUTE_TABLE_FULL):
-            printf("Error: Table full.\n");
-            break;
-        }
-
-    }
+}
 }
